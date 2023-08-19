72b60210f194fb39d1ee3a468517b227c8d52d55
Chandra Prakash J Patel <90178452+bittujpatel@users.noreply.github.com>
2023-08-19 16:44:38 +0530

    Create Rust.rs

---
+++ Rust.rs
@@ -0,0 +1,18 @@
+fn main() {
+   //define a tuple
+    let person_data = ("Alex", 48, "35kg", "6ft");
+   // access value of a tuple
+    println!("The value of the tuple at index 0 and index 1 are {} {}",person_data.0,person_data.1);
+
+   //define a tuple
+    let person_data = ("Bittu", 22, "62kg", "5.4ft");
+   // get individual values out of tuple
+    let (w ,x, y, z) = person_data;
+   //print values
+    println!("Name : {}",w);
+    println!("Age : {}",x);
+    println!("Weight : {}",y);
+    println!("Height : {}",z);
+   //print the value of tuple
+    println!("Tuple - Person Data : {:?}",person_data);
+}
